a50c63c36c8776fa652f7caf8b5fc718223c1d1e
schleeke <schlee.kevin@gmail.com>
2019-06-03 08:54:02 +0200

    Added VS project files

---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApiMock.Startup))]
+namespace WebApiMock {
+    public class Startup {
+
+        public void Configuration(IAppBuilder appBuilder) {
+            appBuilder.Use( async(context, next) => {
+                var httpResult = await WindowsService.GetHttpResponse(context);
+                context.Response.StatusCode = httpResult.StatusCode;
+                context.Response.ContentType = httpResult.ContentType;
+                await context.Response.WriteAsync(httpResult.Content);
+                await next();
+            });
+        }
+
+    }
+}
